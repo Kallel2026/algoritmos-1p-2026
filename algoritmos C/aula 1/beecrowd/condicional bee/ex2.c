fa47ce615bdf429f4553ec9b4b526b3682a386e4
Kallel2026 <antoniokallel15@gmail.com>
2026-04-06 02:34:23 +0000

    save

--- algoritmos C/aula 1/beecrowd/condicional bee/ex2.c
+++ algoritmos C/aula 1/beecrowd/condicional bee/ex2.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <math.h>
+
  
 int main() {
-    double a, b , c, delta, x1, x2;
+    double a, b , c, delta, r1, r2;
     scanf("%lf %lf %lf", &a, &b, &c);
-    delta = pow(b, 2) - 4 * a * c;
-    x1 = (-b + sqrt(delta)) / (2 * a);
-    x2 = (-b - sqrt(delta)) / (2 * a);
-    if ((a == 0) && (delta < 0));
-     {
+    delta = pow(b, 2) - (4 * a * c);
+    if ((2*a==0) || (delta < 0)) {  
         printf("Impossivel calcular\n");
-    }
-    
+    } else {
+        r1 = (-b + sqrt(delta)) / (2 * a);
+        r2 = (-b - sqrt(delta)) / (2 * a);
+        printf("R1 = %.5lf\nR2 = %.5lf\n", r1, r2);
+         }
     return 0;
 }
